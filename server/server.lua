64d4698cb9a2d2dfef4056ecad183db5a324a431
Lenix <149199196+LenixDev@users.noreply.github.com>
2025-06-20 20:58:57 +0100

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -2,6 +2,15 @@ if GetCurrentResourceName() ~= "trplr_weathersync" then
     return print("^6Changing the resource's name wont't let the resource start, ^1" .. GetCurrentResourceName() .. "^0 > ^2 trplr_weathersync ^7")
 end
 
+CreateThread(function()
+   print("  ____              _                      _        ")
+   print(" | __ )   _   _    | |       ___   _ __   (_) __  __")
+   print(" |  _ \\  | | | |   | |     / _ \\  | '_ \\  | | \\\\/ /")
+   print(" | |_) | | |_| |   | |___  | __/  | | | | | |  >  < ")
+   print(" |____/   \\__, |   |_____| \\___|  |_| |_| |_| /_/\\\\ ")
+   print("          |___/                                     ")
+)
+
 local CurrentWeather = Config.StartWeather
 local baseTime = Config.BaseTime
 local timeOffset = Config.TimeOffset
